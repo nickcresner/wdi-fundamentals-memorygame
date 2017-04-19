ad450eb93fea6dfefa647e4eb8e4132796d60d0b
Nick Cresner <nickcresner@Nicks-MBP.lan>
2017-04-19 11:47:07 +0100

    Created game logic

---
+++ memory_game copy/js/main.js
@@ -0,0 +1,25 @@
+console.log("Up and running!");	
+
+
+
+var cards = ["queen", "queen", "king", "king"];	
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+
+}
+
+
+var flipCard = function(cardID) {
+console.log("User flipped " + cards[cardID]);
+cardsInPlay.push(cardID);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
